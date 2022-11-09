9b9b8b7c10094ca8fc878492590e767329ea9d77
Nour Aoude <naoude@gmail.com>
2022-11-09 16:15:37 +0200

    Add draw logic

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const game = (function () {
+    let squaresTaken = 0;
     let board = ["adg", "ae", "afh", "bd", "begh", "bf", "cdh", "ce", "cfg"];
     const winner = document.querySelector(".winner");
-    const _run = (player) => {
-        let sorted = player.squares.split('').sort().join('');
-        return /a{3}|b{3}|c{3}|d{3}|e{3}|f{3}|g{3}|h{3}/.test(sorted);
-    }
     const _togglePlayer = () => {
         if (currentPlayer === 0) currentPlayer = 1;
         else currentPlayer = 0;
@@ -21,21 +18,44 @@ const game = (function () {
         const newRound = document.querySelector(".new-round");
         newRound.innerHTML = '';
         currentPlayer = 0;
+        squares.forEach(square => square.addEventListener("click", go));
         _resetBoard();
     }
     const _declareWinner = (player) => {
         winner.textContent = `${player.name} wins!`
         player.score += 1;
+    }
+    const _endRound = (player) => {
+        if (player) _declareWinner(player);
+        squaresTaken = 0;
+        squares.forEach(square => square.removeEventListener("click", go));
         const newRound = document.querySelector(".new-round");
         const button = document.createElement("button");
         button.textContent = "Play again?";
         newRound.appendChild(button);
         button.addEventListener("click", _newRound);
     }
-    const play = (square) => {
+    const _checkWinner = (player) => {
+        let sorted = player.squares.split('').sort().join('');
+        return /a{3}|b{3}|c{3}|d{3}|e{3}|f{3}|g{3}|h{3}/.test(sorted);
+    }
+    const _run = (square) => {
         players[currentPlayer].squares += board[square];
-        if (_run(players[currentPlayer])) _declareWinner(players[currentPlayer]);
+        squaresTaken++;
+        if (_checkWinner(players[currentPlayer])) _endRound(players[currentPlayer]);
+        else if (squaresTaken === 9) _endRound();
         _togglePlayer();
+    }
+    const _fillSquare = (square) => {
+        if (square.attributes.status.value === "taken") return;
+        if (currentPlayer === 0) square.textContent = "X";
+        else square.textContent = "O";
+        square.attributes.status.value = "taken";
+    }
+    function play(square) {
+        _fillSquare(square);
+        const index = square.attributes.index.value;
+        _run(index);
     }
     const reset = () => players = [];
     return { play, reset };
